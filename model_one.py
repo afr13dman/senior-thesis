21533f350eede0e79e731a764e633d7a49908ca3
spideranne <akfriedman13@gmail.com>
2025-02-23 21:16:41 -0800

    update model one and fix error

--- model_one.py
+++ model_one.py
@@ -2,10 +2,11 @@ import math
 import random 
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 
 rand_seed = 13 #13, 47, 50
-num_vertices = 50
-probs = [2, 2.2, 2.5, 2.6, 2.7]
+num_vertices = 20
+probs = [19.5]
 
 df = pd.DataFrame(columns = ['rand_seed', 'num_vertices', 'prob_base_num', 'avg_deg'])
 
@@ -27,8 +28,8 @@ for p in probs:
     nx.set_node_attributes(G, y_axis, name='y_axis')
 
     # Create edges between points based on distance and probability
-    for i in range(0, 20): # Go from 0 to n-1
-        for j in range(i+1, 20): # Go from i+1 to n-1
+    for i in range(0, num_vertices): # Go from 0 to n-1
+        for j in range(i+1, num_vertices): # Go from i+1 to n-1
             u = (G.nodes()[i]['x_axis'], G.nodes()[i]['y_axis'])
             v = (G.nodes()[j]['x_axis'], G.nodes()[j]['y_axis'])
 
@@ -41,7 +42,13 @@ for p in probs:
             if r < prob: # create an edge
                 G.add_edge(i, j)
     
-    avg_deg = 2 * G.number_of_edges() / G.number_of_nodes()
-    df.loc[len(df)] = [rand_seed, num_vertices, p, avg_deg]
+    # Creating a table to understand avg degree
+    # avg_deg = 2 * G.number_of_edges() / G.number_of_nodes()
+    # df.loc[len(df)] = [rand_seed, num_vertices, p, avg_deg]
 
-df.to_csv("model_one.csv", header=True, index = False)
+    # Create a dictionary of node positions to plot a figure
+    node_locations = {v: (float(G.nodes()[v]['x_axis']), float(G.nodes()[v]['y_axis'])) for v in G.nodes()}
+    nx.draw(G, node_size = 50, pos = node_locations)
+    plt.savefig(f'imgs/model_one/{num_vertices}-{rand_seed}-{p}.png')
+
+# df.to_csv("model_one.csv", header=True, index = False)
